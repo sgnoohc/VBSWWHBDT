b0557e70e5f968672862d169323611e604398955
Philip Chang <sgnoohc@gmail.com>
2021-07-15 19:05:36 -0700

    update

--- apply.sh
+++ apply.sh
@@ -19,8 +19,12 @@ variable_test"
 rm -f .jobs.txt
 for SAMPLE in ${SAMPLES}; do
     for TREE in ${TREES}; do
-        echo "./apply ${SAMPLE} ${TREE}" >> .jobs.txt
+        echo "./apply ${SAMPLE} ${TREE} > output/${SAMPLE}_${TREE}.log 2>&1 " >> .jobs.txt
     done
 done
 
+echo "Launched parallel jobs... "
+
 sh xargs.sh .jobs.txt
+
+echo "Done!"
--- run.sh
+++
@@ -1,8 +0,0 @@
-./train
-./apply bosons variable_test
-./apply raretop variable_test
-./apply ttw variable_test
-./apply ttz variable_test
-./apply tt1lpowheg variable
-./apply tt2lpowheg variable
-./apply vbshww_c2v_4p5 variable_test
